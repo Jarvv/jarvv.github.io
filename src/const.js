d1dedba8726d486185a44d601ad3a8b711d1c56b
James Harvey <james@arcade.ltd>
2024-04-18 21:42:29 +0100

    Updated content

--- src/const.js
+++ src/const.js
@@ -32,64 +32,62 @@ const navLinks = [
 
 const hero = {
     name: 'James',
-    about: `I'm a developer specialising in creating immersive experiences through XR technologies,
-        redefining how we interact and play within the world around us. I have a proven track
-        record of delivering engaging solutions across various sectors, including brands, sports,
-        and cultural institutions.`,
+    about: `I’m a developer specialising in creating immersive experiences through web and AR technologies, with a focus on full-stack development using React, Next.js and Typescript. Proven track record of delivering full-stack solutions for engaging web and AR solutions across various sectors, including brands, sports, and cultural institutions.`,
 }
 
 const skills = [
     {
-        title: 'Unity Developer',
-        icon: unity,
-    },
-    {
-        title: 'WebAR Developer',
-        icon: webAR,
+        title: 'Full Stack Developer',
+        icon: next,
     },
     {
         title: 'Frontend Developer',
         icon: react,
     },
     {
-        title: 'Next.js Developer',
-        icon: next,
+        title: 'WebAR Developer',
+        icon: webAR,
+    },
+    {
+        title: 'Unity Developer',
+        icon: unity,
     },
 ]
 
 const experiences = [
     {
-        title: 'Senior AR Developer',
-        company_name: 'ARCADE',
+        title: 'Senior AR Developer (Full-Stack Focus)',
+        company_name: 'ARCADE XR',
         icon: arcade,
         iconBg: '#3df4de',
         date: 'August 2021 - Current',
         points: [
-            `Setup full CI/CD pipelines for web and Unity projects through both GitHub Actions and Unity Gaming Services using integrated testing frameworks and automated build processes. Improving code quality and deployment efficiency through frequent and reliable app updates.`,
-            `Actively mentored junior developers, encouraging participation through code reviews and fostering a culture of continuous improvements and code ownership.`,
-            `Led the integration of AR experiences through Unity as a Library for existing client applications, collaborating with external developers implementing plugins for seamless integration.`,
+            `Enabled CI/CD pipelines through GitHub Actions, reducing time to market for new features by facilitating frequent and reliable updates.`,
+            `Actively mentored 4 junior developers, encouraging participation through code reviews and fostering a culture of continuous improvements and code ownership.`,
+            `Played a key role in brand campaigns that reached over 130,000 users, enhancing RESTful API data retrieval speeds along critical paths which boosted user engagement time by 15%.`,
+            `Led the architecture and development of a Next.js and Web3 enabled full-stack application for AR-based trails, featuring wallet-based authentication and NFT based progress tracking.`,
         ],
     },
     {
         title: 'AR Developer',
-        company_name: 'ARCADE',
+        company_name: 'ARCADE XR',
         icon: arcade,
         iconBg: '#c2007f',
         date: 'August 2019 - August 2021',
         points: [
-            `Created in-house development tools utilising industry standard design patterns for Unity AR and UI to unify and simplify the development process, enhancing the team's ability for collaboration.`,
-            `Published WebAR projects through 8thWall and ZapWorks, combining frontend expertise with Three.js libraries for physics, particles and shaders.`,
-            `Embraced continuous learning, rapidly adapting to new technologies and methodologies, crucial for the agency's initial growth and success in shaping the development team.`,
+            `Developed multiplayer browser games using React and Node.js, featuring real-time functionality with WebSockets and Redis for server-authoritative actions.`,
+            `Collaborated with design and development teams on WebAR projects for brand activations, using React and Three.js to create engaging user experiences.`,
+            `Embraced continuous learning across new technologies and core business strategies, crucial for the agency's initial growth and instrumental in shaping the development team.`,
         ],
     },
     {
-        title: 'SURE',
+        title: 'Undergraduate Researcher',
         company_name: 'University of Sheffield',
         icon: sheffield,
         iconBg: '#3df4de',
         date: 'June 2018 - September 2018',
         points: [
-            `Worked across a multi-departmental team to create various AR/VR that were used in the University's Festival of the Mind exhibition.`,
+            `Worked across a multi-departmental team to create various AR/VR applications that were used in the University’s Festival of the Mind exhibition.`,
         ],
     },
 ]
@@ -118,28 +116,6 @@ const projects = [
         source_image: github,
     },
     {
-        name: 'Unity MVVM',
-        description:
-            'An example AR furniture app, demonstrating how data binding can be implemented with UI Toolkit.',
-        tags: [
-            {
-                name: 'unity',
-                color: 'blue-text-gradient',
-            },
-            {
-                name: 'c_sharp',
-                color: 'green-text-gradient',
-            },
-            {
-                name: 'design-patterns',
-                color: 'pink-text-gradient',
-            },
-        ],
-        image: editor,
-        source_code_link: 'https://github.com/Jarvv/UI-Toolkit-MVVM',
-        source_image: github,
-    },
-    {
         name: 'The Ritual Of Sakura',
         description:
             'A brand activation built in the 8thWall editor for an in-store popup, featuring physics, particles, states and a robust UI <-> AR event system.',
@@ -160,6 +136,28 @@ const projects = [
         image: rituals,
         source_code_link: 'https://www.8thwall.com/arcade/theritualofsakura',
         source_image: webAR,
+    },
+    {
+        name: 'Unity MVVM',
+        description:
+            'An example AR furniture app, demonstrating how data binding can be implemented with UI Toolkit.',
+        tags: [
+            {
+                name: 'unity',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'c_sharp',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'design-patterns',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: editor,
+        source_code_link: 'https://github.com/Jarvv/UI-Toolkit-MVVM',
+        source_image: github,
     },
 ]
 
